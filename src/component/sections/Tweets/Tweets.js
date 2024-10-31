a4cc9398fcd727d4edd2eef7ff9303cfc80e665b
daniel Kabagambe <danielkabagambe@gmail.com>
2024-10-31 22:09:48 +0300

    TwitterTweetEmbed

--- src/component/sections/Tweets/Tweets.js
+++ src/component/sections/Tweets/Tweets.js
@@ -1,22 +1,12 @@
 import React from "react";
-import { Timeline } from "react-twitter-widgets";
+import { Tweet } from "react-twitter-widgets";
 
-export default function Tweets() {
+export default function TweetEmbed() {
   return (
     <div align="center">
-      <h2>Blog</h2>
-
-      <div className="tweets">
-        <Timeline
-          dataSource={{
-            sourceType: "profile",
-            screenName: "yitamusawo",
-          }}
-          options={{
-            height: "500px",
-            theme: "dark",
-          }}
-        />
+      <h2>Featured Tweet</h2>
+      <div className="tweet-embed">
+        <Tweet tweetId="1851874324094779591" />
       </div>
     </div>
   );
